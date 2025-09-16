46f168b40a6967ead96595ddb51d56a625841e39
JaarmaCo <williamh.jaarma@gmail.com>
2025-09-16 13:46:04 +0200

    Edit on pom.xml (contributors, etc)

--- backend/src/main/java/com/backend/Main.java
+++ backend/src/main/java/com/backend/Main.java
@@ -1,7 +1,11 @@
 package com.backend;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
