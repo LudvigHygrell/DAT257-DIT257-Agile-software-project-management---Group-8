6ce1095aa87cdac7bbcbcfd6345cc6f6e45de3a2
antonkruse <anton.kruse9@gmail.com>
2025-09-18 15:25:56 +0200

    Implemented search bar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from 'react';
 import './styles/App.css';
 import Header from './components/Header.jsx';
+import Searchbar from './components/Searchbar.jsx';
 
 function App() {
+  // State to hold the search request from the Searchbar component
+  const [searchRequest, setSearchRequest] = useState("");
+  
+  // Effect to log the search request whenever it changes
+  const handleSearch = (value) => {setSearchRequest(value); console.log("Search request updated to:", value);};
 
   return (
     <div>
       <Header />
+      <Searchbar placeholder="Search for charity..." userInput={handleSearch} />
       <h1>Hello Test!</h1>
     </div>
   );
