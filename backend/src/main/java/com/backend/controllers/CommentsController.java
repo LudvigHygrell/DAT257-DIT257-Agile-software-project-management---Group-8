8e6983d30097a2b33e5bbd9e9e48e94a5992099d
LudvigHygrell <ludvighygrell@gmail.com>
2025-09-18 15:55:31 +0200

    Mapped Remove and Blame to the Comments Controller

--- backend/src/main/java/com/backend/controllers/CommentsController.java
+++ backend/src/main/java/com/backend/controllers/CommentsController.java
@@ -2,12 +2,7 @@ package com.backend.controllers;
 
 import com.backend.database.CommentsAdapter;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -25,7 +20,7 @@ public class CommentsController {
      * @param json
      * @return
      */
-    @GetMapping("/add")
+    @PostMapping("/add")
     public ResponseEntity<String> add (@RequestBody JsonNode json){
         if(!json.has("comment")){
             return ResponseEntity.badRequest().body("Missing comment text");
@@ -44,6 +39,58 @@ public class CommentsController {
             return ResponseEntity.ok("Comment successfully added");
         }
         return ResponseEntity.status(500).body("Error posting comment");
+    }
 
+    /**
+     *
+     * @param json
+     * @return
+     */
+    @DeleteMapping("/remove")
+    public ResponseEntity<String> remove (@RequestBody JsonNode json){
+        if (!json.has("comment_id")){
+            return ResponseEntity.badRequest().body("Missing comment id");
+        }
+        if (!json.has("charity")){
+            return ResponseEntity.badRequest().body("Missing charity");
+        }
+        String comment_id = json.get("comment_id").asText();
+        String charity = json.get("charity").asText();
+
+        if (CommentsAdapter.remove(comment_id, charity)){
+            return ResponseEntity.ok("Comment successfully removed");
+        }
+        return ResponseEntity.status(404).body("Comment could not be found");
+    }
+
+    /**
+     *
+     * @param json
+     * @return
+     */
+    @PostMapping("/blame")
+    public  ResponseEntity<String> blame (@RequestBody JsonNode json){
+        if (!json.has("comment_id")){
+            return ResponseEntity.badRequest().body("Missing comment id");
+        }
+        if (!json.has("reason")){
+            return ResponseEntity.badRequest().body("Missing reason");
+        }
+        if ((!json.has("blame_victim"))){
+            return ResponseEntity.badRequest().body("Missing blame victim");
+        }
+
+        String comment_id = json.get("comment_id").asText();
+        String reason = json.get("reason").asText();
+        String blame_victim = json.get("blame_victim").asText();
+        String comment = "";
+        if (json.has("comment")){  //Check if the blame contains a "comment" type
+            comment = json.get("comment").asText();
+        }
+        if(CommentsAdapter.blame(comment_id,reason, comment, blame_victim)){
+            return ResponseEntity.ok("Comment successfully blamed");
+        }
+
+        return ResponseEntity.status(500).body("Error producing blame");
     }
 }
--- backend/src/main/java/com/backend/database/CommentsAdapter.java
+++ backend/src/main/java/com/backend/database/CommentsAdapter.java
@@ -4,4 +4,8 @@ public class CommentsAdapter {
     public static boolean add(String comment, String user, String charity){
         return true;
     }
+
+    public static boolean remove(String comment_id, String charity) {return true;}
+
+    public static boolean blame(String comment_id, String reason, String comment, String blame_victim) {return true;}
 }
