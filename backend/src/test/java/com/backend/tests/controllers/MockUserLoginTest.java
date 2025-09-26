a8cf4d63f5bde5b8d956e94ec2a57ebbc547a3a6
JaarmaCo <williamh.jaarma@gmail.com>
2025-09-26 11:26:14 +0200

    Exclude all controllers/** tests from build (requires DB setup)

--- backend/src/test/java/com/backend/tests/controllers/MockUserLoginTest.java
+++ backend/src/test/java/com/backend/tests/controllers/MockUserLoginTest.java
@@ -3,12 +3,12 @@ package com.backend.tests.controllers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import com.backend.tests.ResourceLoader;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,43 +20,45 @@ import org.springframework.test.web.servlet.MvcResult;
  * Tests basic user login functionality.
  * <p>
  * Prerequisites:
- *  1. There exists a table named Users(username, email, userpassword) in the database.
- *  2. The user MockUser99 is not registered.
- *  3. Security permissions are set up to allow /api/users/create without authentication.
+ * 1. There exists a table named Users(username, email, userpassword) in the
+ * database.
+ * 2. The user MockUser99 is not registered.
+ * 3. Security permissions are set up to allow /api/users/create without
+ * authentication.
  */
-@SpringBootTest(webEnvironment=WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 public class MockUserLoginTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-    @Test
-    public void registerLoginDeleteShouldSucceed() throws Exception {
+  @Test
+  public void registerLoginDeleteShouldSucceed() throws Exception {
 
-        mockMvc.perform(post("/api/users/create")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(ResourceLoader.loadJson("register-user-mock.json")
-                            .toPrettyString()))
-                .andExpect(status().isOk());
+    mockMvc.perform(post("/api/users/create")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(ResourceLoader.loadJson("register-user-mock.json")
+            .toPrettyString()))
+        .andExpect(status().isOk());
 
-        MvcResult result = mockMvc.perform(get("/api/users/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(ResourceLoader.loadJson("login-user-mock.json")
-                            .toPrettyString()))
-                .andExpect(status().isOk())
-                .andReturn();
+    MvcResult result = mockMvc.perform(get("/api/users/login")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(ResourceLoader.loadJson("login-user-mock.json")
+            .toPrettyString()))
+        .andExpect(status().isOk())
+        .andReturn();
 
-        String jwtToken = new ObjectMapper()
-                .readTree(result.getResponse()
-                        .getContentAsString())
-                .get("token").asText();
+    String jwtToken = new ObjectMapper()
+        .readTree(result.getResponse()
+            .getContentAsString())
+        .get("token").asText();
 
-        mockMvc.perform(delete("/api/users/remove")
-                        .header("Authorization", "Bearer " + jwtToken)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(ResourceLoader.loadJson("delete-user-mock.json")
-                            .toPrettyString()))
-                .andExpect(status().isOk());
-    }
+    mockMvc.perform(delete("/api/users/remove")
+        .header("Authorization", "Bearer " + jwtToken)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(ResourceLoader.loadJson("delete-user-mock.json")
+            .toPrettyString()))
+        .andExpect(status().isOk());
+  }
 }
